3d49c4923476896826f0a0af8c9412c5412f92a3
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2012-02-10 20:06:38 +0100

    Add azy test stuff

--- src/lib/ems_server.c
+++ src/lib/ems_server.c
@@ -6,14 +6,14 @@ void ems_server_init(void)
 {
    Azy_Server *serv;
    Azy_Server_Module_Def **mods;
-   
+
    azy_init();
    //Define the list of module used by the server.
    Azy_Server_Module_Def *modules[] = {
      EMS_Config_module_def(),
      NULL
    };
-   
+
    serv = azy_server_new(EINA_FALSE);
    azy_server_addr_set(serv, "0.0.0.0");
    azy_server_port_set(serv, 2000);
@@ -22,7 +22,7 @@ void ems_server_init(void)
         if (!azy_server_module_add(serv, *mods))
           ERR("Unable to create server\n");
      }
-
+   INF("Start Azy server");
    azy_server_start(serv);
-   
+
 }
