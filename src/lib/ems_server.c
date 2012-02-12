41021611b9bccbc524ababfab256fe7d7f28cf1c
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2012-02-12 17:46:55 +0100

    Use azy_server_run as a replacement for ecore_main_loop_begin

--- src/lib/ems_server.c
+++ src/lib/ems_server.c
@@ -6,9 +6,11 @@
 
 #include "EMS_Config.azy_server.h"
 
+
 /*============================================================================*
  *                                  Local                                     *
  *============================================================================*/
+Azy_Server *_serv;
 
 /*============================================================================*
  *                                 Global                                     *
@@ -16,7 +18,7 @@
 
 void ems_server_init(void)
 {
-   Azy_Server *serv;
+
    Azy_Server_Module_Def **mods;
 
    azy_init();
@@ -26,16 +28,20 @@ void ems_server_init(void)
      NULL
    };
 
-   serv = azy_server_new(EINA_FALSE);
-   azy_server_addr_set(serv, "0.0.0.0");
-   azy_server_port_set(serv, 2000);
+   _serv = azy_server_new(EINA_FALSE);
+   azy_server_addr_set(_serv, "0.0.0.0");
+   azy_server_port_set(_serv, ems_config->port);
    for (mods = modules; mods && *mods; mods++)
      {
-        if (!azy_server_module_add(serv, *mods))
+        if (!azy_server_module_add(_serv, *mods))
           ERR("Unable to create server\n");
      }
+}
+
+void ems_server_run(void)
+{
    INF("Start Azy server");
-   azy_server_start(serv);
+   azy_server_run(_serv);
 }
 
 /*============================================================================*
