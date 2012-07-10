d56ca3d3bbe3eaf4eabe38338a9e36ff1b89fb5d
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2012-07-10 19:31:02 +0200

    Back to default version for the theme

--- src/bin/enna/enna_config.c
+++ src/bin/enna/enna_config.c
@@ -90,7 +90,7 @@ enna_config_theme_get(void)
      return theme;
 
    snprintf(tmp, sizeof(tmp),
-            PACKAGE_DATA_DIR"/theme/desktop.edj");
+            PACKAGE_DATA_DIR"/theme/default.edj");
 
    INF("Theme : %s", tmp);
 
