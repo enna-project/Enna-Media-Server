9572be66a60d608a7f3fdeffa2bf2f6ae730e463
Cedric BAIL <cedric.bail@samsung.com>
2012-08-24 19:40:58 +0900

    ecore_con_eet: add ecore_con_eet_raw_send.

--- src/lib/ecore_con_eet.c
+++ src/lib/ecore_con_eet.c
@@ -2,7 +2,10 @@
 
 #include "Ecore_Con_Eet.h"
 
+#define ECORE_CON_EET_RAW_MAGIC 0xDEAD007
+
 typedef struct _Ecore_Con_Eet_Data Ecore_Con_Eet_Data;
+typedef struct _Ecore_Con_Eet_Raw_Data Ecore_Con_Eet_Raw_Data;
 typedef struct _Ecore_Con_Eet_Client Ecore_Con_Eet_Client;
 typedef struct _Ecore_Con_Eet_Server Ecore_Con_Eet_Server;
 
@@ -12,11 +15,24 @@ struct _Ecore_Con_Reply
    Ecore_Con_Client *client;
 
    Eet_Connection *econn;
+
+   char *buffer_section;
+   unsigned char *buffer;
+   unsigned int buffer_length;
+   unsigned int buffer_current;
+   Ecore_Con_Eet_Raw_Data *buffer_handler;
 };
 
 struct _Ecore_Con_Eet_Data
 {
    Ecore_Con_Eet_Data_Cb func;
+   const char *name;
+   const void *data;
+};
+
+struct _Ecore_Con_Eet_Raw_Data
+{
+   Ecore_Con_Eet_Raw_Data_Cb func;
    const char *name;
    const void *data;
 };
@@ -45,6 +61,7 @@ struct _Ecore_Con_Eet
    Eet_Data_Descriptor *matching;
 
    Eina_Hash *data_callbacks;
+   Eina_Hash *raw_data_callbacks;
 
    union {
       struct {
@@ -73,6 +90,24 @@ _ecore_con_eet_data_free(void *data)
    free(eced);
 }
 
+static void
+_ecore_con_eet_raw_data_free(void *data)
+{
+   Ecore_Con_Eet_Raw_Data *eced = data;
+
+   eina_stringshare_del(eced->name);
+   free(eced);
+}
+static void
+_ecore_con_eet_reply_cleanup(Ecore_Con_Reply *n)
+{
+   if (n->buffer_handler) free(n->buffer);
+   n->buffer = NULL;
+   n->buffer_handler = NULL;
+   free(n->buffer_section);
+   n->buffer_section = NULL;
+}
+
 typedef struct _Ecore_Con_Eet_Protocol Ecore_Con_Eet_Protocol;
 struct _Ecore_Con_Eet_Protocol {
    const char *type;
@@ -82,7 +117,7 @@ struct _Ecore_Con_Eet_Protocol {
 static const char *
 _ecore_con_eet_data_type_get(const void *data, Eina_Bool *unknow EINA_UNUSED)
 {
-   Ecore_Con_Eet_Protocol *p = data;
+   const Ecore_Con_Eet_Protocol *p = data;
 
    return p->type;
 }
@@ -214,6 +249,85 @@ _ecore_con_eet_server_disconnected(void *data, int type EINA_UNUSED, Ecore_Con_E
    return EINA_TRUE;
 }
 
+static void
+_ecore_con_eet_raw_data_push(Ecore_Con_Reply *n, void *data, int size)
+{
+   if (n->buffer_handler)
+     memcpy(n->buffer + n->buffer_current, data, size);
+   n->buffer_current += size;
+
+   if (n->buffer_current == n->buffer_length)
+     {
+        if (n->buffer_handler)
+          n->buffer_handler->func((void*) n->buffer_handler->data, n, n->buffer_handler->name, n->buffer_section, n->buffer, n->buffer_length);
+        _ecore_con_eet_reply_cleanup(n);
+     }
+}
+
+static void
+_ecore_con_eet_data(Ecore_Con_Reply *n, void *data, unsigned int size)
+{
+   /* FIXME: Enforce detection of attack and kill connection on that case */
+
+   if (n->buffer)
+     {
+        if (n->buffer_current + size > n->buffer_length)
+          {
+             _ecore_con_eet_reply_cleanup(n);
+             return ;
+          }
+
+        _ecore_con_eet_raw_data_push(n, data, size);
+        return ;
+     }
+   else if (eet_connection_empty(n->econn) && size > (int) (4 * sizeof (unsigned int) + 2))
+     {
+        unsigned int *tmp = data;
+        size -= 4 * sizeof (unsigned int) + 2;
+
+        if (ntohl(tmp[0]) == ECORE_CON_EET_RAW_MAGIC)
+          {
+             unsigned int protocol_length = ntohl(tmp[1]);
+             unsigned int section_length = ntohl(tmp[2]);
+             unsigned int data_length = ntohl(tmp[3]);
+
+             if (protocol_length > 1 && section_length > 1 && protocol_length + section_length <= size && data_length < 10 * 1024 * 1024)
+               {
+                  char *buffer = (char*) &tmp[4];
+                  char *protocol;
+                  char *section;
+
+                  protocol = buffer;
+                  section = buffer + protocol_length;
+
+                  if (protocol[protocol_length - 1] == '\0' &&
+                      section[section_length - 1] == '\0')
+                    {
+                       size -= protocol_length + section_length;
+                       buffer = section + section_length;
+
+                       n->buffer_handler = eina_hash_find(n->ece->raw_data_callbacks, protocol);
+                       n->buffer_section = strdup(section);
+                       n->buffer_length = data_length;
+                       n->buffer_current = 0;
+                       if (n->buffer_handler)
+                         n->buffer = malloc(sizeof (data_length));
+                       else
+                         n->buffer = (void*) 1;
+                       if (n->buffer)
+                         {
+                            _ecore_con_eet_raw_data_push(n, buffer, size);
+                            return ;
+                         }
+                       _ecore_con_eet_reply_cleanup(n);
+                    }
+               }
+          }
+     }
+
+   eet_connection_received(n->econn, data, size);
+}
+
 static Eina_Bool
 _ecore_con_eet_server_data(void *data, int type EINA_UNUSED, Ecore_Con_Event_Client_Data *ev)
 {
@@ -225,7 +339,7 @@ _ecore_con_eet_server_data(void *data, int type EINA_UNUSED, Ecore_Con_Event_Cli
 
    n = ecore_con_client_data_get(ev->client);
 
-   eet_connection_received(n->econn, ev->data, ev->size);
+   _ecore_con_eet_data(n, ev->data, ev->size);
 
    return EINA_TRUE;
 }
@@ -294,7 +408,7 @@ _ecore_con_eet_client_data(void *data, int type EINA_UNUSED, Ecore_Con_Event_Ser
    if (!r->u.client.r) return EINA_TRUE;
 
    /* Got some data */
-   eet_connection_received(r->u.client.r->econn, ev->data, ev->size);
+   _ecore_con_eet_data(r->u.client.r, ev->data, ev->size);
 
    return EINA_TRUE;
 }
@@ -317,6 +431,7 @@ ecore_con_eet_server_new(Ecore_Con_Server *server)
    r->handler_data = ecore_event_handler_add(ECORE_CON_EVENT_CLIENT_DATA,
                                              (Ecore_Event_Handler_Cb)_ecore_con_eet_server_data, r);
    r->data_callbacks = eina_hash_stringshared_new(_ecore_con_eet_data_free);
+   r->raw_data_callbacks = eina_hash_stringshared_new(_ecore_con_eet_raw_data_free);
 
    _ecore_con_eet_data_descriptor_setup(r);
 
@@ -342,6 +457,7 @@ ecore_con_eet_client_new(Ecore_Con_Server *server)
    r->handler_data = ecore_event_handler_add(ECORE_CON_EVENT_SERVER_DATA,
                                              (Ecore_Event_Handler_Cb)_ecore_con_eet_client_data, r);
    r->data_callbacks = eina_hash_stringshared_new(_ecore_con_eet_data_free);
+   r->raw_data_callbacks = eina_hash_stringshared_new(_ecore_con_eet_raw_data_free);
 
    _ecore_con_eet_data_descriptor_setup(r);
 
@@ -356,6 +472,7 @@ ecore_con_eet_server_free(Ecore_Con_Eet *r)
    eet_data_descriptor_free(r->edd);
    eet_data_descriptor_free(r->matching);
    eina_hash_free(r->data_callbacks);
+   eina_hash_free(r->raw_data_callbacks);
 
    if (r->client)
      {
@@ -363,6 +480,7 @@ ecore_con_eet_server_free(Ecore_Con_Eet *r)
 
         if (r->u.client.r)
           {
+             _ecore_con_eet_reply_cleanup(r->u.client.r);
              eet_connection_close(r->u.client.r->econn, NULL);
              free(r->u.client.r);
           }
@@ -378,6 +496,7 @@ ecore_con_eet_server_free(Ecore_Con_Eet *r)
 
         EINA_LIST_FREE(r->u.server.connections, n)
           {
+             _ecore_con_eet_reply_cleanup(n);
              eet_connection_close(n->econn, NULL);
              free(n);
           }
@@ -423,6 +542,37 @@ ecore_con_eet_data_callback_del(Ecore_Con_Eet *ece, const char *name)
 {
    if (!ece) return ;
    eina_hash_del(ece->data_callbacks, name, NULL);
+}
+
+void
+ecore_con_eet_raw_data_callback_add(Ecore_Con_Eet *ece, const char *name, Ecore_Con_Eet_Raw_Data_Cb func, const void *data)
+{
+   Ecore_Con_Eet_Raw_Data *eced;
+
+   if (!ece) return ;
+
+   eced = calloc(1, sizeof (Ecore_Con_Eet_Raw_Data));;
+   if (!eced) return ;
+
+   eced->func = func;
+   eced->data = data;
+   eced->name = eina_stringshare_add(name);
+
+   eina_hash_direct_add(ece->raw_data_callbacks, eced->name, eced);   
+}
+
+void
+ecore_con_eet_raw_data_callback_del(Ecore_Con_Eet *ece, const char *name)
+{
+   if (!ece) return ;
+
+   if (ece->client && ece->u.client.r->buffer_handler && !strcmp(ece->u.client.r->buffer_handler->name, name))
+     {
+        ece->u.client.r->buffer_handler = NULL;
+        free(ece->u.client.r->buffer);
+        ece->u.client.r->buffer = (void*) 1;
+     }
+   eina_hash_del(ece->raw_data_callbacks, name, NULL);
 }
 
 void
@@ -592,3 +742,44 @@ ecore_con_eet_send(Ecore_Con_Reply *reply, const char *name, void *value)
 
    eet_connection_send(reply->econn, reply->ece->edd, &protocol, NULL);
 }
+
+void
+ecore_con_eet_raw_send(Ecore_Con_Reply *reply, const char *protocol_name, const char *section, void *value, unsigned int length)
+{
+   unsigned int protocol[4];
+   unsigned int protocol_length;
+   unsigned int section_length;
+   unsigned int size;
+   char *tmp;
+
+   if (!reply) return ;
+   if (!protocol_name) return ;
+   if (!section) return ;
+
+   protocol_length = strlen(protocol_name) + 1;
+   if (protocol_length == 1) return ;
+   section_length = strlen(section) + 1;
+
+   protocol[0] = htonl(ECORE_CON_EET_RAW_MAGIC);
+   protocol[1] = htonl(protocol_length);
+   protocol[2] = htonl(section_length);
+   protocol[3] = htonl(length);
+
+   size = sizeof (protocol) + protocol_length + section_length;
+   tmp = alloca(size);
+   memcpy(tmp, protocol, sizeof (protocol));
+   memcpy(tmp + sizeof (protocol), protocol, protocol_length);
+   memcpy(tmp + sizeof (protocol) + protocol_length, section, section_length);
+
+   if (reply->client)
+     {
+        ecore_con_client_send(reply->client, tmp, size);
+        ecore_con_client_send(reply->client, value, length);
+     }
+   else
+     {
+        ecore_con_server_send(reply->ece->server, tmp, size);
+        ecore_con_server_send(reply->ece->server, value, length);
+     }
+}
+
