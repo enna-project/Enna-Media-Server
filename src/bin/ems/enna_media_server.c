fca4273b654ecd7f6bb5b9f8edfdc4d25ca73a14
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2012-12-05 23:04:10 +0100

    Add -n option to start in no daemonize mode

--- src/bin/ems/enna_media_server.c
+++ src/bin/ems/enna_media_server.c
@@ -23,44 +23,48 @@ int main(int argc, char **argv)
    int fp;
    char str[32];
 
-   pid = fork();
-   if (pid < 0)
-     return EXIT_FAILURE;
+   if (argc == 1)
+     {
 
-   if (pid > 0)
-     return EXIT_SUCCESS;
+       pid = fork();
+       if (pid < 0)
+	 return EXIT_FAILURE;
+
+       if (pid > 0)
+	 return EXIT_SUCCESS;
  
-   umask(027);
+       umask(027);
 
-   sid = setsid();
-   if (sid < 0)
-     return EXIT_FAILURE;
+       sid = setsid();
+       if (sid < 0)
+	 return EXIT_FAILURE;
         
         
-   if ((chdir("/")) < 0)
-     return EXIT_FAILURE;
+       if ((chdir("/")) < 0)
+	 return EXIT_FAILURE;
         
-   /* Close out the standard file descriptors */
-   close(STDIN_FILENO);
-   close(STDOUT_FILENO);
-   close(STDERR_FILENO);
+       /* Close out the standard file descriptors */
+       close(STDIN_FILENO);
+       close(STDOUT_FILENO);
+       close(STDERR_FILENO);
 
 
-   fp = open("/run/lock/enna-media-server.lock", 
-             O_RDWR|O_CREAT, 0640);
-   printf("Fp : %d\n", fp);
-   if (fp < 0)
-     return EXIT_FAILURE;
+       fp = open("/tmp/enna-media-server.lock", 
+		 O_RDWR|O_CREAT, 0640);
+       printf("Fp : %d\n", fp);
+       if (fp < 0)
+	 return EXIT_FAILURE;
 
-   if (lockf(fp, F_TLOCK, 0) < 0) 
-     return EXIT_FAILURE;
+       if (lockf(fp, F_TLOCK, 0) < 0) 
+	 return EXIT_FAILURE;
 
-   snprintf(str, sizeof(str), "%d\n", getpid());
-   write(fp, str, strlen(str)); /* record pid to lockfile */
-
+       snprintf(str, sizeof(str), "%d\n", getpid());
+       write(fp, str, strlen(str)); /* record pid to lockfile */
+     }
+   
    if (!ems_init(NULL))
      return EXIT_FAILURE;
-        
+   ems_avahi_start();
    ems_run();
    ems_shutdown();
    return EXIT_SUCCESS;
