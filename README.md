cd456d4260fbb0f992d6baebee7eca64c195ca2f
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2015-03-28 12:35:54 +0100

    Remove old code

---
+++ README.md
@@ -0,0 +1,12 @@
+Enna Media Server version 0.1
+
+Enna Media Server, is a daemon application, which runs in background and collect informations about your medias.
+
+Needs QT 5 and above
+
+For building it : 
+qmake ems.pro
+make
+make install
+
+
--- src/lib/server/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CFLAGS="-Wall -Wextra -O0 -g -ggdb3 $(pkg-config --libs --cflags azy ecore eina)"
-
-colorgcc $CFLAGS-o client client.c ems_rpc_Config.azy_client.c ems_rpc_Browser.azy_client.c ems_rpc_Common.c ems_rpc_Common_Azy.c $CFLAGS
-
