fd1172e51b3b60ba53c91150de0ffa33b6f559e7
Vincent Dehors <vincent.dehors@openwide.fr>
2015-03-30 18:42:38 +0200

    ems: add new command in the test web pgm

--- tests/app/scripts/main.js
+++ tests/app/scripts/main.js
@@ -22,6 +22,7 @@ var apiList = [
     '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "shuffle_off"}',
     '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "repeat_on"}',
     '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "repeat_off"}',
+    '{ "msg": "EMS_PLAYLIST", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "add", "url": "playlist://current", "filename": "cdda://"}',
     '{ "msg": "EMS_PLAYLIST", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "add", "url": "playlist://current", "filename": "file:///media/usb/music/beatles"}',
     '{ "msg": "EMS_PLAYLIST", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "del", "url": "playlist://current", "filename": "file:///media/usb/music/beatles"}',
     '{ "msg": "EMS_PLAYLIST", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "clear", "url": "playlist://current"}',
