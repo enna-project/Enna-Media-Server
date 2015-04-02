0c170db55e7c77a31d70903138f031509cb5dff9
Vincent Dehors <vincent.dehors@openwide.fr>
2015-04-02 15:28:50 +0200

    ems/tests: add new command in the test app
    Conflicts:
    	ems/tests/dist/main.js

--- tests/dist/scripts/main.js
+++ tests/dist/scripts/main.js
@@ -1,1 +1,222 @@
-"use strict";function popuplateApiList(){for(var a=0,b=0;b<apiList.length;b++){var c=JSON.parse(apiList[b]).msg,d=JSON.parse(apiList[b]).url,e=JSON.parse(apiList[b]).action;d&&(c+=" | "+d),e&&(c+=" | "+e),$("#api_list").append($("<option />").val(a++).html(c))}$("#api_list").change(function(){var a=$("#api_list option:selected").val(),b=apiList[parseInt(a)];$("#message").val(JSON.stringify(JSON.parse(b),null,"    "))}),$("#api_list").change()}function addLog(a,b){b="undefined"!=typeof b?b:!1;var c=loginfos.length,d=$("<p></p>");loginfos.push(a),$(".log").append(d),"connected"==a.type?(d.append($('<span class="glyphicon glyphicon-ok">')),d.append("Connected")):"disconnected"==a.type?(d.append($('<span class="glyphicon glyphicon-remove">')),d.append("Disconnected")):"error"==a.type?(d.append($('<span class="glyphicon glyphicon-remove-sign">')),d.append(a.type)):b?(d.append($('<span class="glyphicon glyphicon-export">')),d.append(""==a.type?'Empty "msg"':a.type)):(d.append($('<span class="glyphicon glyphicon-import">')),d.append(""==a.type?'Empty "msg"':a.type)),$(".badge").html(loginfos.length),d.on("click",function(){var a,b=loginfos[c];try{a=JSON.stringify(JSON.parse(b.json),null,"    ")}catch(d){a=b.json}$("#answer").html(a),$("pre code").each(function(a,b){hljs.highlightBlock(b)})})}function clearLog(){loginfos=[],$(".log").html(""),$(".badge").html("0")}var apiList=['{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music"}','{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/artists"}','{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/artists/1"}','{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/artists/1/1"}','{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/albums"}','{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/albums/1"}','{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/tracks"}','{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/genres"}','{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/genres/1"}','{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "next"}','{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "prev"}','{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "play"}','{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "pause"}','{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "toggle"}','{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "stop"}','{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "shuffle_on"}','{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "shuffle_off"}','{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "repeat_on"}','{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "repeat_off"}'],ws=new Object,loginfos=[];$(document).ready(function(){popuplateApiList(),$("#urlinput").val("ws://127.0.0.1:7337"),$("#btdisconnect").on("click",function(){ws.close()}),$("#btsend").on("click",function(){var a=$("#message").val();if(!("readyState"in ws&&ws.readyState==ws.OPEN))return console.log("sending error"),void addLog({type:"error",msg_id:"",json:"Websocket connection closed"});ws.send(a);var b=JSON.parse(a),c="",d="UNKNOWN!!";b.hasOwnProperty("msg_id")&&(c=b.msg_id),b.hasOwnProperty("msg")&&(d=b.msg),addLog({type:d,msg_id:c,json:a},!0)}),$("#btclear").on("click",function(){clearLog()}),$("#btconnect").on("click",function(){ws=new WebSocket($("#urlinput").val()),ws.onopen=function(){$("#status_con").removeClass("label-danger"),$("#status_con").addClass("label-success"),$("#status_con").html("Connected"),addLog({type:"connected",msg_id:"",json:"OPEN"})},ws.onclose=function(){$("#status_con").removeClass("label-success"),$("#status_con").addClass("label-danger"),$("#status_con").html("Disconnected"),addLog({type:"disconnected",msg_id:"",json:"CLOSED"})},ws.onmessage=function(a){console.log(a.data);var b=JSON.parse(a.data),c="",d="UNKNOWN!!";b.hasOwnProperty("msg_id")&&(c=b.msg_id),b.hasOwnProperty("msg")&&(d=b.msg),addLog({type:d,msg_id:c,json:a.data})},ws.onerror=function(a){$("#status_con").removeClass("label-success"),$("#status_con").addClass("label-danger"),$("#status_con").html("Disconnected"),addLog({type:"error",msg_id:"",json:a.data})}})});
+'use strict';
+
+var apiList = [
+    '{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "menu://"}',
+    '{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "cdda://"}',
+    '{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music"}',
+    '{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/artists"}',
+    '{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/artists/1"}',
+    '{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/artists/1/1"}',
+    '{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/albums"}',
+    '{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/albums/1"}',
+    '{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/tracks"}',
+    '{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/genres"}',
+    '{ "msg": "EMS_BROWSE", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "url": "library://music/genres/1"}',
+    '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "next"}',
+    '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "prev"}',
+    '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "play"}',
+    '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "pause"}',
+    '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "toggle"}',
+    '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "stop"}',
+    '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "shuffle_on"}',
+    '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "shuffle_off"}',
+    '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "repeat_on"}',
+    '{ "msg": "EMS_PLAYER", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "repeat_off"}',
+    '{ "msg": "EMS_PLAYLIST", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "add", "url": "playlist://current", "filename": "cdda://"}',
+    '{ "msg": "EMS_PLAYLIST", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "add", "url": "playlist://current", "filename": "file:///media/usb/music/beatles"}',
+    '{ "msg": "EMS_PLAYLIST", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "add", "url": "playlist://current", "filename": "library://music/tracks/1"}',
+    '{ "msg": "EMS_PLAYLIST", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "add", "url": "playlist://current", "filename": "library://music/albums/1"}',
+    '{ "msg": "EMS_PLAYLIST", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "del", "url": "playlist://current", "filename": "file:///media/usb/music/beatles"}',
+    '{ "msg": "EMS_PLAYLIST", "msg_id": "42", "uuid": "110e8400-e29b-11d4-a716-446655440000", "action": "clear", "url": "playlist://current"}',
+];
+
+function popuplateApiList() {
+    var i = 0;
+    for (var c = 0;c < apiList.length;c++) {
+        var s = JSON.parse(apiList[c]).msg
+        var url = JSON.parse(apiList[c]).url
+        var action = JSON.parse(apiList[c]).action
+        if (url)
+            s += ' | ' + url
+        if (action)
+            s += ' | ' + action
+        $('#api_list').append($('<option />').val(i++).html(s));
+    }
+
+    $('#api_list').change(function() {
+        var j = $('#api_list option:selected').val();
+        var s = apiList[parseInt(j)];
+        $('#message').val(JSON.stringify(JSON.parse(s), null, '    '));
+    });
+
+    $('#api_list').change();
+}
+
+var ws = new Object();
+var loginfos = [];
+
+function addLog(o, sending) {
+
+    sending = typeof sending !== 'undefined' ? sending : false;
+
+    var id = loginfos.length;
+    var newItem = $('<p></p>');
+    loginfos.push(o);
+
+    $('.log').append(newItem);
+    if (o.type == 'connected') {
+        newItem.append($('<span class="glyphicon glyphicon-ok">'));
+        newItem.append('Connected');
+    }
+    else if (o.type == 'disconnected') {
+        newItem.append($('<span class="glyphicon glyphicon-remove">'));
+        newItem.append('Disconnected');
+    }
+    else if (o.type == 'error') {
+        newItem.append($('<span class="glyphicon glyphicon-remove-sign">'));
+        newItem.append(o.type);
+    }
+    else if (sending) {
+        newItem.append($('<span class="glyphicon glyphicon-export">'));
+        if (o.type == '')
+            newItem.append('Empty "msg"');
+        else
+            newItem.append(o.type);
+    }
+    else {
+        newItem.append($('<span class="glyphicon glyphicon-import">'));
+        if (o.type == '')
+            newItem.append('Empty "msg"');
+        else
+            newItem.append(o.type);
+    }
+
+    $('.badge').html(loginfos.length);
+
+    newItem.on('click', function () {
+        var data = loginfos[id];
+        var txt;
+        try {
+            txt = JSON.stringify(JSON.parse(data.json), null, '    ');
+        }
+        catch(ex) {
+            txt = data.json;
+        }
+
+        $('#answer').html(txt);
+
+        $('pre code').each(function(i, block) {
+          hljs.highlightBlock(block);
+        });
+    });
+}
+
+function clearLog() {
+    loginfos = [];
+    $('.log').html('');
+    $('.badge').html('0');
+}
+
+$(document).ready(function() {
+
+    popuplateApiList();
+
+    $('#urlinput').val('ws://127.0.0.1:7337');
+
+    $('#btdisconnect').on('click', function () {
+        ws.close();
+    });
+
+    $('#btsend').on('click', function () {
+
+        var m = $('#message').val();
+
+        if ('readyState' in ws &&
+             ws.readyState == ws.OPEN) {
+            ws.send(m);
+        }
+        else {
+            console.log('sending error');
+            addLog({
+                type: 'error',
+                msg_id: '',
+                json: 'Websocket connection closed'
+            });
+            return;
+        }
+
+        var json = JSON.parse(m);
+
+        var id = '';
+        var msg = 'UNKNOWN!!';
+        if (json.hasOwnProperty('msg_id'))
+            id = json.msg_id;
+        if (json.hasOwnProperty('msg'))
+            msg = json.msg;
+
+        addLog({
+            type: msg,
+            msg_id: id,
+            json: m
+        }, true);
+    });
+
+    $('#btclear').on('click', function () {
+        clearLog();
+    });
+
+    $('#btconnect').on('click', function () {
+        ws = new WebSocket($('#urlinput').val());
+        ws.onopen = function(evt) {
+            $('#status_con').removeClass('label-danger');
+            $('#status_con').addClass('label-success');
+            $('#status_con').html('Connected');
+
+            addLog({
+                type: 'connected',
+                msg_id: '',
+                json: 'OPEN'
+            });
+        };
+        ws.onclose = function(evt) {
+            $('#status_con').removeClass('label-success');
+            $('#status_con').addClass('label-danger');
+            $('#status_con').html('Disconnected');
+
+            addLog({
+                type: 'disconnected',
+                msg_id: '',
+                json: 'CLOSED'
+            });
+        };
+        ws.onmessage = function(evt) {
+            console.log(evt.data);
+
+            var json = JSON.parse(evt.data);
+
+            var id = '';
+            var msg = 'UNKNOWN!!';
+            if (json.hasOwnProperty('msg_id'))
+                id = json.msg_id;
+            if (json.hasOwnProperty('msg'))
+                msg = json.msg;
+
+            addLog({
+                type: msg,
+                msg_id: id,
+                json: evt.data
+            });
+        };
+        ws.onerror = function(evt) {
+            $('#status_con').removeClass('label-success');
+            $('#status_con').addClass('label-danger');
+            $('#status_con').html('Disconnected');
+
+            addLog({
+                type: 'error',
+                msg_id: '',
+                json: evt.data
+            });
+        };
+    });
+});
